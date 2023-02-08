72827d7c7b95af6fc877642a7e906e66e4260e7b
Hafil Noer Zachiary <hafilzachiary@gmail.com>
2023-02-09 01:08:55 +0800

    init

---
+++ src/main/java/sg/edu/smu/cs301/group3/campaignms/CampaignMsApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.smu.cs301.group3.campaignms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/smu/cs301/group3/campaignms/CampaignMsApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.smu.cs301.group3.campaignms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
