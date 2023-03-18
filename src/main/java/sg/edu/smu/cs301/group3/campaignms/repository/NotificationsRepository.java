1ecdcfae52750bb1642879b9c0a049d419a4a467
levi85 <joshua.zhangzy@gmail.com>
2023-03-18 21:54:46 +0800

    updated repo to just JPA instead of JDBC

--- src/main/java/sg/edu/smu/cs301/group3/campaignms/repository/NotificationsRepository.java
+++ src/main/java/sg/edu/smu/cs301/group3/campaignms/repository/NotificationsRepository.java
@@ -1,11 +1,11 @@
 package sg.edu.smu.cs301.group3.campaignms.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sg.edu.smu.cs301.group3.campaignms.model.Notification;
 
 import java.util.List;
 
-public interface NotificationsRepository extends CrudRepository<Notification, Long> {
+public interface NotificationsRepository extends JpaRepository<Notification, Long> {
     List<Notification> getNotificationsByCampaignId(int campaignId);
     void deleteAllByCampaignId(int campaignId);
 }
