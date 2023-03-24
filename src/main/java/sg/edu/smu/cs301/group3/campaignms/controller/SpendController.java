6c26424ca51c719abcf1bae23e42a1881761d360
StanfordDC <stanforddchandra@gmail.com>
2023-03-24 20:24:47 +0800

    Updated file names and url

--- src/main/java/sg/edu/smu/cs301/group3/campaignms/controller/SpendController.java
+++ src/main/java/sg/edu/smu/cs301/group3/campaignms/controller/SpendController.java
@@ -5,31 +5,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import sg.edu.smu.cs301.group3.campaignms.model.Reward;
-import sg.edu.smu.cs301.group3.campaignms.model.SpendObject;
-import sg.edu.smu.cs301.group3.campaignms.service.SpendObjectService;
+import sg.edu.smu.cs301.group3.campaignms.beans.RewardBean;
+import sg.edu.smu.cs301.group3.campaignms.beans.SpendBean;
+import sg.edu.smu.cs301.group3.campaignms.service.SpendService;
 
 @RestController
-public class SpendObjectController {
+public class SpendController {
 
-    private SpendObjectService spendObjectService;
+    private SpendService spendService;
 
     @Autowired
     private QueueMessagingTemplate queueMessagingTemplate;
 
     @Value("${aws.campaign.to.card.queue.url}")
-    private String endpoint;
+    private String campaignToCardQueueUrl;
 
-    public void sendMessage(Reward reward){
-        queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(reward).build());
+    public void sendMessage(RewardBean reward){
+        queueMessagingTemplate.send(campaignToCardQueueUrl, MessageBuilder.withPayload(reward).build());
     }
 
     @GetMapping("/convert")
-    public void convertAndSend(@RequestBody SpendObject spendObject){
-        Reward reward = spendObjectService.convertToReward(spendObject);
+    public void convertAndSend(@RequestBody SpendBean spendBean){
+        RewardBean reward = spendService.convertToReward(spendBean);
         sendMessage(reward);
     }
 }
